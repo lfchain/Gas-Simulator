687c4e23488413afaf158915250db508b948c81d
Fucheng Li <lfucheng123@gmail.com>
2018-12-21 10:44:39 -0500

    First Commit

---
+++ ParticleSim/src/main/java/ParticleSim/ParticleSim/App.java
@@ -0,0 +1,13 @@
+package ParticleSim.ParticleSim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
